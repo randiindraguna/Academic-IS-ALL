c0fc8c1cd089cb20183a66bfcc957c8ba517aab4
pakaditya <adityaanggaramadhan@gmail.com>
2019-05-22 13:06:04 +0700

    170018131_Aditya Angga Ramadhan_ 6 file di pengelola_ fix

---
+++ pengelola/pencarian_mahasiswa_diadmin.php
@@ -0,0 +1,70 @@
+<?php 
+ 
+ session_start();
+if($_SESSION['status'] == "login"){
+  // menampilkan pesan selamat datang
+  //echo "Hai, selamat datang ". $_SESSION['username'];
+}else{
+  header("location:../index.php");
+}
+
+?>
+<?php include '../templates/header_Penjadwalan.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../templates/navbar_admin.html' ?>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- /\ambil css penjadwalan -->
+    <!-- Tambahan CSS -->
+    <link rel="stylesheet" href="../css/style_penjadwalan.css">
+    <link rel="stylesheet" href="../css/switches_Penjadwalan.css">
+
+    <style type="text/css" href="../css/tombol_penjadwalan.css"></style>
+</head>
+<body>
+
+
+
+    
+   
+   
+    <!-- Content -->
+        <div class="container"> 
+            <!-- Box -->
+            <div class="row mt-5 ">
+                <div class="col-2">
+                </div>
+                <div class="col-8 box2 bg-two">
+                    <div class="row">
+                        <div class="col-10 mt-3 mb-3">
+                            <p class="judul">Seminar Proposal</p>
+                        </div>
+                    </div>
+                    <form action="hasil_pencarian_diadmin.php" method="POST">
+                        <div class="row">
+                            <div class="col-2 ml-3 pt-1">
+                                <p class="pone">NIM :</p>
+                            </div>
+                            <div class="col-6">
+                                <input type="text" name='nim' placeholder='Masukkan NIM' class="form-control in-box" name="nim">
+                            </div>
+                            <div class="col-2 mb-5">
+                                <button type="submit" name="submit" value="Submit" class="butn butn2 ml-2" >Search
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-2 ">
+            </div>
+        </div>
+        
+</body>
+</html>
+<?php include '../templates/footer_Penjadwalan.php' ?>
---
+++ pengelola/update_pendadaran_diadmin.php
@@ -0,0 +1,93 @@
+<?php include '../templates/header_Penjadwalan.php' ?>
+<?php
+
+
+	//membutuhkan file fungsi_semprop
+	require('../fungsi_pendadaran.php');
+
+	//instansiasi objek class Seminar_Proposal
+	$akses = new ujian_pendadaran();
+	$akses->koneksi();
+session_start();
+if($_SESSION['status'] == "login"){
+  // menampilkan pesan selamat datang
+  //echo "Hai, selamat datang ". $_SESSION['username'];
+}else{
+  header("location:../index.php");
+
+}
+?>
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../templates/navbar_admin.html' ?>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- /\ambil css penjadwalan -->
+    <!-- Tambahan CSS -->
+    <link rel="stylesheet" href="../css/style_penjadwalan.css">
+    <link rel="stylesheet" href="../css/switches_Penjadwalan.css">
+
+    <style type="text/css" href="../css/tombol_penjadwalan.css"></style>
+</head>
+<body>
+<?php
+  
+$nim=$_GET['nim'];
+     
+      foreach ($akses->UpdateNilaiDanStatusPendadaran2($nim) as $key) {
+        
+        
+
+
+        echo"
+        <br>
+        <h2 align='center'>Data mahasiswa</h2>
+        <br>
+        <table align='center'>
+        <tr>
+        <td width='700px'>
+<form method='POST' action='proses_updatependadaran_diadmin.php'>
+  <div class='form-group'>
+    <label for='formGroupExampleInput'>NIM </label>
+    <input name='nim' value='$key[nim]' type='text' readonly class='form-control' id='formGroupExampleInput' placeholder='Example input'>
+    <label for='formGroupExampleInput'>NAMA </label>
+    <input name='nama' value='$key[nama_mhs]' type='text' readonly class='form-control' id='formGroupExampleInput' placeholder='Example input'>
+
+  </div>
+ 
+        
+        
+        ";
+
+
+                    
+        echo "
+        <label for='formGroupExampleInput'>NILAI PENGUJI 1 </label><input type='text' name='nilai_penguji_1' class='form-control' aria-label='Text input with checkbox'>
+         <label for='formGroupExampleInput'>NILAI PENGUJI 2 </label><input type='text' name='nilai_penguji_2' class='form-control' aria-label='Text input with checkbox'>
+          <label for='formGroupExampleInput'>NILAI PEMBIMBING </label><input type='text' name='nilai_pembimbing' class='form-control' aria-label='Text input with checkbox'>
+       <label for='formGroupExampleInput'>STATUS </label><select name='status' class='form-control' aria-label='Text input with checkbox' >
+                                         <option>- PILIH -</option> 
+                                        <option value='lulus'> lulus </option> 
+                                        <option value='tidak_lulus'> tidak lulus </option>
+                                   </select> 
+        
+        <br>   <input type='submit' name='kirim1' value='kirim' class='btn btn-outline-primary''>    
+        
+
+        </form>
+        </td>
+        </tr>
+        </table>      ";
+
+
+      
+    }
+      ?>
+<?php include '../templates/footer_Penjadwalan.php' ?>
+
+

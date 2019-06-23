86dc69a7430d76a75248b56bad2c6ec6cfc5a393
Agung321 <agungparmono8@gmail.com>
2019-06-24 01:45:29 +0700

    ubah desain sedikit di admin menu mahasiswa

--- pengelola/semester.php
+++ pengelola/semester.php
@@ -70,14 +70,14 @@ include '../templates/header_penjadwalan.php';
 
    <?php include '../templates/navbar_admin.html'?>
 <table border="0" width="100%"  height="100%" align= "center">
-  <tr><td colspan="3" bgcolor="#808080"><br></td></tr>
+  <tr><td colspan="3" ><br></td></tr>
   <tr>
-    <td width="25%" bgcolor="#808080" rowspan="2">
+    <td width="25%" rowspan="2">
     </td>
     <td width="50%">
       <table cellpadding="20"width="100%" border="0"  height="100%">
         <tr>
-          <td bgcolor="#F5F5F5">
+          <td bgcolor="#B5B5B5" style="width: 100%;height: 100%;border-radius: 20px;padding-top: 20px;padding-bottom: 20px;box-shadow: 0px 0px 5px 2px #d1d1d1;">
             <center><h3>Pengelolaan Semester</h3></center>
             <form action="semester.php" method="POST">
               <div class="form-group">
@@ -122,7 +122,7 @@ include '../templates/header_penjadwalan.php';
                        <td>$data[periode]</td>
                        <td>$data[status]</td>
                        <td><a href='update_semester.php?id_semester=$data[id_semester]'>Update</a></td>
-                       <td><a href='delete_semester.php?id_semester=$data[id_semester]'>delete</a></td>
+                       <td><a href='delete_semester.php?id_semester=$data[id_semester]'>Delete</a></td>
                     </tr>";
                       $i = $i+1;
                           }
@@ -137,15 +137,15 @@ include '../templates/header_penjadwalan.php';
         </tr>
       </table>
     </td>
-    <td width="25%" bgcolor="#808080" rowspan="2"></td>
+    <td width="25%"  rowspan="2"></td>
   </tr> 
 </table>
 <table cellpadding="27" border="0" width="100%" height="20%">
   <tr align="center">
-    <td bgcolor="#808080">
-      <div  id="footer" style="height:50px; line-height:50px; background:#333; color:white;">
+    <td >
+      <div  id="footer" style="height:50px; line-height:50px; background:#333; color:white;border-radius: 30px;">
         Copyright &copy; 2019
-        Designed by . . . . . . . .
+        Designed by Team Register Metopen
       </div>
     </td>
   </tr>

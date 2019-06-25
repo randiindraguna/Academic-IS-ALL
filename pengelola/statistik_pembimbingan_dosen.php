a2d8700788105ee14e158a55468a28ded3c18e90
ervinfm <ervinfm14@gmail.com>
2019-06-25 20:08:25 +0700

    menambahkan statistik pembimbingan dosen

---
+++ pengelola/statistik_pembimbingan_dosen.php
@@ -0,0 +1,140 @@
+<?php include '../templates/header_Penjadwalan.php' ?>
+<?php 
+include "database_statistik.php";
+    $akses = new Analitik();
+    $akses->connect();
+ ?>
+<?php
+ session_start();
+if($_SESSION['status'] == "login"){
+  // menampilkan pesan selamat datang
+  //echo "Hai, selamat datang ". $_SESSION['username'];
+}else{
+  header("location:../index.php");
+
+}
+?>
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../templates/navbar_admin.html' ?>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--grafik-->
+   <script type="text/javascript" src="chartjs/Chart.js"></script>
+    <!--grafik-->
+    <!-- /\ambil css penjadwalan -->
+    <!-- Tambahan CSS -->
+    <link rel="stylesheet" href="../css/style_penjadwalan.css">
+    <link rel="stylesheet" href="../css/switches_Penjadwalan.css">
+
+    <style type="text/css" href="../css/tombol_penjadwalan.css"></style>
+</head>
+<body>
+    <!-- Content -->
+        <div class="container"> 
+            <!-- Box -->
+           <!--  <div class="row mt-5 ">
+                <div class="col-2">
+                </div>
+                <div class="col-8 box2 bg-two">
+                    <div class="row">
+                        <div class="col-10 mt-3 mb-3">
+                            <p class="judul">Ujian Pendadaran</p>
+                        </div>
+                    </div>
+                    <form action="hasil_pencarian_PD_diadmin.php" method="POST">
+                        <div class="row">
+                            <div class="col-2 ml-3 pt-1">
+                                <p class="pone">NIM :</p>
+                            </div>
+                            <div class="col-6">
+                                <input type="text" name='nim' placeholder='Masukkan NIM' class="form-control in-box" name="nim">
+                            </div>
+                            <div class="col-2 mb-5">
+                                <button type="submit" name="submit11" value="Submit" class="butn butn2 ml-2" >Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-2 ">
+            </div> -->
+            <!--Grafik-->
+        </br>.
+            <h2 class="judul"><center>Statistik Pembimbingan Dosen </center></h2>
+            <h2 class="judul"><center>Berdasarkan Bidang minat Mahasiswa </center></h2>
+            <?php
+             foreach($akses->jumlah_ampu_bimbingan() as $key){
+                echo '"'.$key['jumlah_ampu'].'",';
+            }
+            foreach($akses->jumlah_ampu_bimbingan() as $key){
+                echo '"'.$key['dos_bing'].'",';
+            }
+
+             ?>
+        <br>
+       <div style="width: 1000px;margin: 0px auto;">
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script>
+    var i;
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [<?php foreach($akses->jumlah_ampu_bimbingan() as $key){echo '"'.$key['dos_bing'].'",';}?>],
+                datasets: [{
+                    label: '',
+                    data: [
+                    <?php 
+                        foreach($akses->jumlah_ampu_bimbingan() as $key){echo '"'.$key['jumlah_ampu'].'",';}
+                    ?>,
+                    ],
+                    
+                    backgroundColor: [
+                    
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(80, 99, 132, 0.2)',
+                            'rgba(255, 20, 132, 0.2)',
+                            'rgba(255, 255, 144, 0.2)',
+                            'rgba(190, 20, 56, 0.2)',
+                            'rgba(255, 150, 255, 0.2)'
+                            
+                    ],
+                    borderColor: [
+
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(80, 99, 132, 0.2)',
+                            'rgba(255, 20, 132, 0.2)',
+                            'rgba(255, 255, 144, 0.2)',
+                            'rgba(190, 20, 56, 0.2)',
+                            'rgba(255, 150, 255, 0.2)'
+
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+            <!--Grafik-->
+        </div>
+    
+    </body>
+</html>
+        
+
+<?php include '../templates/footer_Penjadwalan.php' ?>

559a48bb3aa2259e140ca19ff2537b28393ce188
afynu27 <danu.yolandanu@gmail.com>
2019-06-28 00:29:58 +0700

    memodifikasi button login dengan Google API oleh Abdun fattah yolandanu 1700018168

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
             <label class="form-check-label" for="showpass">Show Password</label>
           </div>
           <button type="submit" class="btn btn-primary">Login</button>
-          <input type="button" onclick="window.location = 'google_acc.php';" value="Log in dengan google" class="btn btn-danger">
+          <button type="submit" formaction="google_acc.php" class="btn btn-outline-danger"><img src="img/logo_google+.png" height=22px> Login dengan google</button>
          <a href="formInputAkun.php" class="btn btn-warning">Daftar</a>
         </form>
         </div>

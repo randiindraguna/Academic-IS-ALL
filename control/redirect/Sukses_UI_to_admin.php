67c01a444cbcd6f3b824d528bbf2e824ade2fbeb
Adhymas Fajar Sudrajad <48277426+dmonh3h3@users.noreply.github.com>
2019-06-29 12:57:30 +0700

    Fix Bug session

--- control/redirect/Sukses_UI_to_admin.php
+++ control/redirect/Sukses_UI_to_admin.php
@@ -1,7 +1,11 @@
 <?php
 ?>
 <form id="my_form" action="../pengelola/UI_Penjadwalan_admin.php" method="post">
-
+	<input type="hidden" name="user" value="admin">
+	<?php 
+	session_start();
+	$_SESSION['username']=NULL;
+	?>
 </form>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script type="text/javascript">

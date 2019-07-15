6229b15b2d19ada16dd81de58a40061b4c60587b
nandasuci <48277932+nandasuci@users.noreply.github.com>
2019-07-15 08:48:11 +0700

    add kondisi cek nim terdaftar
    di pendadaran

--- control/edit_semprop_ke_pendadaran.php
+++ control/edit_semprop_ke_pendadaran.php
@@ -23,47 +23,50 @@ $akses ->connect();
     $nilai_ruangDS=$akses->cekRuangDalamSehari($ruang,$tanggal);
 // Cek Ruang Waktu Dalam Sehari
     $nilai_ruangwaktudlmsehari=$akses->cekRuangWaktuDalamSehari($ruang,$waktu,$tanggal);
+// cek nim
+    $cek_nim = $akses->cekNimdataYangSama($nim,"UNDARAN");
 
     // Insert to Database
-    if ($nilai_penguji1==true) {
+    if($cek_nim==true){
         if ($nilai_penguji1==true) {
-            if ($cekDosenJamTanggalSamaBedaRuang1==true) {
-                if ($cekDosenJamTanggalSamaBedaRuang2==true) {
-                    if ($nilai_waktuDS==true) {
-                        if ($nilai_ruangDS==true) {
-                            if ($nilai_ruangwaktudlmsehari==true) {
-                                // $akses->eksekusi("SET foreign_key_checks = 0");
-                                // $akses->UpdateTabelPenjadwalanByIdJadwal($idjadwal,$idjadwal_baru,"UNDARAN",$nim,$tanggal,$waktu,$ruang);
-                                // $akses->eksekusi("SET foreign_key_checks = 0");
-                                // $akses->UpdateTabelPengujiByIdJadwal($idjadwal,$idjadwal_baru,$penguji1,$penguji1);
-                                // $akses->eksekusi("SET foreign_key_checks = 1");
-                                // $akses->insertPenguji($idjadwal_baru,$penguji2);
-                                $akses->insertJadwal($idjadwal_baru,"UNDARAN",$nim,$tanggal,$waktu,$ruang);
-                                $akses->insertPenguji($idjadwal_baru,$penguji1);
-                                $akses->insertPenguji($idjadwal_baru,$penguji2);
+            if ($nilai_penguji1==true) {
+                if ($cekDosenJamTanggalSamaBedaRuang1==true) {
+                    if ($cekDosenJamTanggalSamaBedaRuang2==true) {
+                        if ($nilai_waktuDS==true) {
+                            if ($nilai_ruangDS==true) {
+                                if ($nilai_ruangwaktudlmsehari==true) {
+                                    // $akses->eksekusi("SET foreign_key_checks = 0");
+                                    // $akses->UpdateTabelPenjadwalanByIdJadwal($idjadwal,$idjadwal_baru,"UNDARAN",$nim,$tanggal,$waktu,$ruang);
+                                    // $akses->eksekusi("SET foreign_key_checks = 0");
+                                    // $akses->UpdateTabelPengujiByIdJadwal($idjadwal,$idjadwal_baru,$penguji1,$penguji1);
+                                    // $akses->eksekusi("SET foreign_key_checks = 1");
+                                    // $akses->insertPenguji($idjadwal_baru,$penguji2);
+                                    $akses->insertJadwal($idjadwal_baru,"UNDARAN",$nim,$tanggal,$waktu,$ruang);
+                                    $akses->insertPenguji($idjadwal_baru,$penguji1);
+                                    $akses->insertPenguji($idjadwal_baru,$penguji2);
 
-                                ?>
-                                <form id="my_form" action="../pengelola/UI_Penjadwalan_detail_admin.php" method="GET">
-                                <input type="hidden" name="nim" value="<?=$nim;?>">
-                                </form>
-                                <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-                                <script type="text/javascript">
-                                    function alert() {
-                                    swal("Selamat!", "Data Anda Berhasil Diubah!", "success")
-                                    .then((value) => {simpan();})
-                                    }
-                                    function simpan() {
-                                    document.getElementById('my_form').submit();   
-                                    }                            
-                                    window.onload = alert;
-                                </script> 
-                                <?php
-                            }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang & waktu Penuh";}
-                        }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang Penuh";}
-                    }else {include 'redirect/redirect_failed_to_back.php';echo "Waktu Penuh";}
+                                    ?>
+                                    <form id="my_form" action="../pengelola/UI_Penjadwalan_detail_admin.php" method="GET">
+                                    <input type="hidden" name="nim" value="<?=$nim;?>">
+                                    </form>
+                                    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+                                    <script type="text/javascript">
+                                        function alert() {
+                                        swal("Selamat!", "Data Anda Berhasil Diubah!", "success")
+                                        .then((value) => {simpan();})
+                                        }
+                                        function simpan() {
+                                        document.getElementById('my_form').submit();   
+                                        }                            
+                                        window.onload = alert;
+                                    </script> 
+                                    <?php
+                                }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang & waktu Penuh";}
+                            }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang Penuh";}
+                        }else {include 'redirect/redirect_failed_to_back.php';echo "Waktu Penuh";}
+                    }else {include 'redirect/redirect_failed_to_back.php';echo "Dosen Sudah Ada Di ruang lain";}
                 }else {include 'redirect/redirect_failed_to_back.php';echo "Dosen Sudah Ada Di ruang lain";}
-            }else {include 'redirect/redirect_failed_to_back.php';echo "Dosen Sudah Ada Di ruang lain";}
+            }else {include 'redirect/redirect_failed_to_back.php';echo "Penguji Penuh";}
         }else {include 'redirect/redirect_failed_to_back.php';echo "Penguji Penuh";}
-    }else {include 'redirect/redirect_failed_to_back.php';echo "Penguji Penuh";}
-
+    }else {include 'redirect/redirect_failed_to_back.php';echo "Nim Sudah terdaftar";}
 ?>

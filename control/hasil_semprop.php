da1a6a2b092a6e387bc5544dfcb2f861e5b5b7f7
nandasuci <48277932+nandasuci@users.noreply.github.com>
2019-07-15 08:49:24 +0700

    add cek nim terdaftar di semprop

--- control/hasil_semprop.php
+++ control/hasil_semprop.php
@@ -21,20 +21,23 @@ $idjadwal  = $akses->createIdJadwal("SP", $penguji1, $penguji2, $tanggal, $waktu
     $nilai_ruangDS=$akses->cekRuangDalamSehari($ruang,$tanggal);
 // Cek Ruang Waktu Dalam Sehari
     $nilai_ruangwaktudlmsehari=$akses->cekRuangWaktuDalamSehari($ruang,$waktu,$tanggal);
+// cek nim
+    $cek_nim = $akses->cekNimdataYangSama($nim,"SEMPROP");
+
 // Insert to Database
-
-    if ($nilai_penguji1==true) {
-            if ($nilai_waktuDS==true) {
-                if ($nilai_ruangDS==true) {
-                    if ($cekDosenJamTanggalSamaBedaRuang==true) {
-                        if ($nilai_ruangwaktudlmsehari==true) {
-                            $akses->insertJadwal($idjadwal,"SEMPROP",$nim,$tanggal,$waktu,$ruang);
-                            $akses->insertPenguji($idjadwal,$penguji1);
-                            include 'redirect/Sukses_UI_to_admin.php';
-                        }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang & waktu Penuh";}
-                    }else {include 'redirect/redirect_failed_to_back.php';echo "Dosen Sudah Ada Di ruang lain";}
-                }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang Penuh";}
-            }else {include 'redirect/redirect_failed_to_back.php';echo "Waktu Penuh";}
-    }else {include 'redirect/redirect_failed_to_back.php';echo "Penguji Penuh";}
-
+    if($cek_nim==true){
+        if ($nilai_penguji1==true) {
+                if ($nilai_waktuDS==true) {
+                    if ($nilai_ruangDS==true) {
+                        if ($cekDosenJamTanggalSamaBedaRuang==true) {
+                            if ($nilai_ruangwaktudlmsehari==true) {
+                                $akses->insertJadwal($idjadwal,"SEMPROP",$nim,$tanggal,$waktu,$ruang);
+                                $akses->insertPenguji($idjadwal,$penguji1);
+                                include 'redirect/Sukses_UI_to_admin.php';
+                            }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang & waktu Penuh";}
+                        }else {include 'redirect/redirect_failed_to_back.php';echo "Dosen Sudah Ada Di ruang lain";}
+                    }else {include 'redirect/redirect_failed_to_back.php';echo "Ruang Penuh";}
+                }else {include 'redirect/redirect_failed_to_back.php';echo "Waktu Penuh";}
+        }else {include 'redirect/redirect_failed_to_back.php';echo "Penguji Penuh";}
+    }else {include 'redirect/redirect_failed_to_back.php';echo "Nim Sudah terdaftar";}
 ?>

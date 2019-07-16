aff7a1757964adaaab8ee98d67d1e4a51dfc6757
febrirama <febriramadhan45@gmail.com>
2019-07-16 14:54:38 +0700

    menambahkan allert

--- pengelola/proses_update_diadmin.php
+++ pengelola/proses_update_diadmin.php
@@ -1,12 +1,13 @@
 <?php include '../templates/header_Penjadwalan.php' ?>
+
 <?php
 
-	//membutuhkan file fungsi_semprop
-	require('../fungsi_semprop.php');
+  //membutuhkan file fungsi_semprop
+  require('../fungsi_semprop.php');
 
-	//instansiasi objek class Seminar_Proposal
-	$akses = new Seminar_Proposal();
-	$akses->koneksi();
+  //instansiasi objek class Seminar_Proposal
+  $akses = new Seminar_Proposal();
+  $akses->koneksi();
 
    session_start();
 if($_SESSION['status'] == "login"){
@@ -17,8 +18,7 @@ if($_SESSION['status'] == "login"){
 }
 
 ?>
-
-
+<?php include '../templates/header_Penjadwalan.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,14 +35,23 @@ if($_SESSION['status'] == "login"){
     <link rel="stylesheet" href="../css/switches_Penjadwalan.css">
 
     <style type="text/css" href="../css/tombol_penjadwalan.css"></style>
+
+      <script type="text/javascript" src="../mahasiswa/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <script type="text/javascript" src="../mahasiswa/sweetalert2/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../mahasiswa/sweetalert2/dist/sweetalert2.min.css">
 </head>
+
 <?php
- $id_s = $_POST['nim'];
+  
+if(isset($_POST['simpan'])){
+
+    $id = $_POST['nim'];
     $nilai_pb = $_POST['nilai_proses_pembimbing'];
+    // $status = $_POST['status'];
     $nilai_ub = $_POST['nilai_ujian_pembimbing'];
     $nilai_up = $_POST['nilai_ujian_penguji'];
 
-    $rata_rata=round(($nilai_pb+$nilai_ub+$nilai_up)/3,2);
+$rata_rata=round(($nilai_pb+$nilai_ub+$nilai_up)/3,2);
  if($rata_rata>-1 && $rata_rata<=1) $grade='E';
   else if($rata_rata>0 && $rata_rata<=40) $grade='D';
   else if($rata_rata>40 && $rata_rata<=43.75) $grade='D+';
@@ -57,153 +66,4 @@ if($_SESSION['status'] == "login"){
   else $grade='nilai tidak tersedia'; 
 
   if($rata_rata>51.25) $status='lulus';
-  else($status='tidak_lulus')  ;  
-
-      $akses->UpdateNilaiDanStatusSemprop1($nilai_pb,$status,$id_s,$nilai_ub,$nilai_up);
-?>
-
-<br>
-
-    <h2 align="center">DATA SEMINAR PROPOSAL</h2>
-    <table width='90%'>
-        <th>
-          <td align='right' > <a target="_blank" href="export_excel_semprop.php">EXPORT KE EXCEL</a><br></td>
-          
-          </th>
-      </table>
-<br>
-
-  
-<br>
-<table align="center">
-<form name="pencarian" method="POST" action = "hasil_cari_pengunguman_diadmin.php" ">            
-      
-                    <tr> <td>
-                    <input type="text" placeholder="masukan nim" name="nim" title ="masukan nim" class="form-control">  
-                    </td>
-                    <td>
-                    <button id="submit2" name="submit2" class='butn butn2 ml-2'>cari</button></td>
-                   </tr>
-
-          </form>
-        </table>
-    
-<?php
-      foreach ($akses->HitungJumlahMahasiswaLulusSemprop() as $key) {
-        echo"
-      <table width='90%'>
-        <th>
-          <td align='right' > Jumlah yang lulus : $key[lulus]</td>
-          
-          </th>
-      </table>
-        ";
-
-
-      
-    }   
-      ?>
-
-<?php
-      foreach ($akses->HitungJumlahMahasiswaTidakLulusSemprop() as $key) {
-        echo"
-      <table width='90%'>
-        <th>
-           <td align='right' > Jumlah yang tidak lulus : $key[tidak_lulus]</td>
-          
-          </th>
-      </table>
-        ";
-
-
-      
-    }
-
-        
-      ?>
-      <br>
-
-
-
-
-
-
-
-<table border='1' align='center' width='95%'' height='30%'>
-    <tr align='center' bgcolor='#D3D3D3'>
-      <th height='50'>Nim</th>
-      <th height='50' >Nama</th>
-      <th height='50'>Nilai Proses pembimbing</th>
-       <th height='50'>Nilai ujian pembimbing</th>
-        <th height='50'>Nilai ujian penguji</th>
-         <th height='50'>Rata-rata</th>
-          <th height='50'>Grade</th>
-
-      <th height='50'>Status</th>
-      <th height='50'>Action</th>
-    </tr>
-
-
-<?php
-    
- foreach ($akses->LihatPengumumanNilaiDanStatusSemuaMahasiswa() as $key) {
-
- #DIBUAT OLHE IBRAHIM
-$rata_rata=round(($key['nilai_proses_pembimbing']+$key['nilai_ujian_pembimbing']+$key['nilai_ujian_penguji'])/3,2);
- if($rata_rata>-1 && $rata_rata<=1) $grade='E';
-  else if($rata_rata>0 && $rata_rata<=40) $grade='D';
-  else if($rata_rata>40 && $rata_rata<=43.75) $grade='D+';
-  else if($rata_rata>43.75 && $rata_rata<=51.25) $grade='C-';
-  else if($rata_rata>51.25 && $rata_rata<=55) $grade='C';
-  else if($rata_rata>55 && $rata_rata<=57.5) $grade='C+';
-  else if($rata_rata>57.5 && $rata_rata<=62.5) $grade='B-';
-  else if($rata_rata>62.5 && $rata_rata<=65) $grade='B';
-  else if($rata_rata>65 && $rata_rata<=68.75) $grade='B+';
-  else if($rata_rata>68.75 && $rata_rata<=76.25) $grade='A-';
-  else if($rata_rata>76.25 && $rata_rata<=100) $grade='A';
-  else $grade='nilai tidak tersedia'; 
-
-  if($rata_rata>51.25) $status='lulus';
-  else($status='tidak_lulus')  ;     
-        echo "
-        <tr>
-          <td align='center'>$key[nim]</td>
-          <td align='center'>$key[nama_mhs]</td>
-          <td align='center'>$key[nilai_proses_pembimbing]</td>
-           <td align='center'>$key[nilai_ujian_pembimbing]</td>
-            <td align='center'>$key[nilai_ujian_penguji]</td>
-          <td align='center'>$rata_rata</td>
-          <td align='center'>$grade</td>
-          <td align='center'>$key[status]</td>
-          <td align='center'><a href='update_semrop_diadmin.php?nim=$key[nim]' role='button' class='btn btn-outline-primary'>EDIT</a>
-          <a href='delete_semprop_diadmin.php?nim=$key[nim]' role='button' class='btn btn-outline-primary'>DELETE</a></td>
-          </tr>
-         
-        ";
-
-
-      
-    }
-
-    
-
-
-      ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>javascript-- pesan</title>
-  <script type="text/javascript">
-    function pesan(){
-      alert("DATA ANDA TELAH TERSIMPAN TERIMA KASIH")
-    }
-  </script>
-</head> 
-<body onload="pesan()">
-  
-
-</body>
-</html>
- 
-<?php include '../templates/footer_Penjadwalan.php' ?>
+  else($status='tidak_lulus')  ;

8ffc606a3e377124d6bfcc05d94d02c8889fb7e7
afynu27 <danu.yolandanu@gmail.com>
2019-07-16 16:48:10 +0700

    mengatasi error

--- kelolaAkun.php
+++ kelolaAkun.php
@@ -1,10 +1,13 @@
 <?php 
 include 'database.php';
-$login = new Database();
-$login->connect();
+include 'class_login.php';
+$login1 = new Database();
+$login1->connect();
+$login2 = new Login();
+$login2->connect();
 
-foreach ($login->getDosen() as $key) {
-	$password = $login->create_random_password(8);
-	$login->insert_tabel_login($key['niy'],$password,'dosen','baru');
+foreach ($login1->getDosen() as $key) {
+	$password = $login2->create_random_password(8);
+	$login2->insert_tabel_login($key['niy'],$password,'dosen','baru');
 }
 ?>
--- pengelola/list_password_dosen.php
+++ pengelola/list_password_dosen.php
@@ -1,4 +1,5 @@
 <?php 
+require_once('../kelolaAkun.php');
 require_once('../class_login.php');
 
   $akses = new Login();

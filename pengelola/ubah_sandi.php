377053478ff5898417144d5ecc60c164c2fe8db0
Nophand <nofandgokil@gmail.com>
2019-07-25 13:54:08 +0700

    [UAS - Nofand Adlil M - 1700018152] -> membenarkan buq/error di fitur ubah password

--- pengelola/ubah_sandi.php
+++ pengelola/ubah_sandi.php
@@ -1,5 +1,5 @@
 <?php
-	require_once 'class_login.php';
+	require_once '../class_login.php';
 	$akses = new Login();
  	$akses->connect();
  	session_start();
@@ -13,14 +13,14 @@
  	$akunRow = mysqli_num_rows($akses->searchAkun($user,$oldpass));
 
  	if($akunRow<=0){
- 		 header("location:profil_mahasiswa.php?fail_chg");
+ 		 header("location:profil_pengelola.php?fail_chg");
  	}
  	else if($newpass!=$ulangpass){
- 		 header("location:profil_mahasiswa.php?fail_chg");
+ 		 header("location:profil_pengelola.php?fail_chg");
  	}
  	else{
  			$akses->ganti_password($user,$newpass,'dirubah');
- 			 header("location:profil_mahasiswa.php?can_chg");
+ 			 header("location:profil_pengelola.php?can_chg");
  	}
 
 ?>
